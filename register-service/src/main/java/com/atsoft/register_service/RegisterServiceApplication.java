2b929d57a052532ac0a2a7ba2ca5a8b7108bb7c9
ahmed diallo <tidiane.mqp@gmail.com>
2025-07-26 04:12:39 +0200

    complete register-service

--- register-service/src/main/java/com/atsoft/register_service/RegisterServiceApplication.java
+++ register-service/src/main/java/com/atsoft/register_service/RegisterServiceApplication.java
@@ -3,7 +3,6 @@ package com.atsoft.register_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
 @EnableEurekaServer
 @SpringBootApplication
 public class RegisterServiceApplication {
